57840acd606df59594da15f24b2ef86b5e741a01
Vitaly Kharlakov <vkharlakov@yandex.ru>
2023-10-03 12:58:11 +0200

    Fix data

---
+++ lib/data/filterMenuData/data.ts
@@ -0,0 +1,52 @@
+export const filters = [
+  {
+    title: "Seasons",
+    options: [
+      {
+        title: "Winter",
+        hash: "#winter",
+        icon: null,
+      },
+      {
+        title: "Spring",
+        hash: "#spring",
+        icon: null,
+      },
+      {
+        title: "Summer",
+        hash: "#summer",
+        icon: null,
+      },
+      {
+        title: "Autumn",
+        hash: "#autumn",
+        icon: null,
+      },
+    ],
+  },
+  {
+    title: "Time",
+    options: [
+      {
+        title: "Night",
+        hash: "#night",
+        icon: null,
+      },
+      {
+        title: "Dawn",
+        hash: "#dawn",
+        icon: null,
+      },
+      {
+        title: "Day",
+        hash: "#day",
+        icon: null,
+      },
+      {
+        title: "Dusk",
+        hash: "#dusk",
+        icon: null,
+      },
+    ],
+  },
+];
